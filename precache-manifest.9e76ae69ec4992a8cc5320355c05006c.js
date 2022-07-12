eaf04cbe4e86dcd3840fd26a470ebaa3ee4c18fb
yuval-david <yuvid0224@gmail.com>
2022-07-12 03:05:58 +0300

    Updates

---
+++ precache-manifest.9e76ae69ec4992a8cc5320355c05006c.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "7567701b1e06f2676ad6",
+    "url": "/portfolio/static/css/main.5dc7dde8.chunk.css"
+  },
+  {
+    "revision": "7567701b1e06f2676ad6",
+    "url": "/portfolio/static/js/main.7567701b.chunk.js"
+  },
+  {
+    "revision": "cfbbcc48d29ad874e737",
+    "url": "/portfolio/static/js/1.cfbbcc48.chunk.js"
+  },
+  {
+    "revision": "d798a6362bad6794128e",
+    "url": "/portfolio/static/css/2.4e881008.chunk.css"
+  },
+  {
+    "revision": "d798a6362bad6794128e",
+    "url": "/portfolio/static/js/2.d798a636.chunk.js"
+  },
+  {
+    "revision": "548806b00eeab4bea233",
+    "url": "/portfolio/static/js/runtime~main.548806b0.js"
+  },
+  {
+    "revision": "3df66fdf2655f4e59b78ceb0e5efa18b",
+    "url": "/portfolio/static/media/angular-icon.3df66fdf.png"
+  },
+  {
+    "revision": "7d4fe56a906707466de4e066e8269719",
+    "url": "/portfolio/static/media/css.7d4fe56a.png"
+  },
+  {
+    "revision": "9999c1a8c347b81c5cb5df967664c4c3",
+    "url": "/portfolio/static/media/jquery.9999c1a8.png"
+  },
+  {
+    "revision": "efd22267efa1c9589fe5611454dfe821",
+    "url": "/portfolio/static/media/mysql.efd22267.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/portfolio/static/media/node.55e83371.png"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/portfolio/static/media/react-icon.22808bae.png"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/portfolio/static/media/github-c.b6ec19ce.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/portfolio/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "d492efc706db983e74258dbd348f2208",
+    "url": "/portfolio/static/media/linkedin-c.d492efc7.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/portfolio/static/media/mail-c.2c1a7560.png"
+  },
+  {
+    "revision": "36179cab6c72e89b07b0f115bb2fc2e4",
+    "url": "/portfolio/static/media/mail.36179cab.png"
+  },
+  {
+    "revision": "0185186e818d5e7ee3fdb1150f0fdc8e",
+    "url": "/portfolio/static/media/pink-calling.0185186e.gif"
+  },
+  {
+    "revision": "3bdae00bec1c2a8d718c8cbfcf943e30",
+    "url": "/portfolio/static/media/html5-icon.3bdae00b.png"
+  },
+  {
+    "revision": "da07e2241f00fb9f29081b1fe0f5a3a3",
+    "url": "/portfolio/static/media/jquery-logo.da07e224.png"
+  },
+  {
+    "revision": "af0dad0aff317476578e26326f7e565a",
+    "url": "/portfolio/static/media/mysql-icon.af0dad0a.png"
+  },
+  {
+    "revision": "f2e31b365ff924fd8b2e7db7b13ee44a",
+    "url": "/portfolio/static/media/socket-io.f2e31b36.jpeg"
+  },
+  {
+    "revision": "29945216c35d5ed413a5079d3e370292",
+    "url": "/portfolio/static/media/yuval.29945216.jpg"
+  },
+  {
+    "revision": "0734aff79a8af321fb865c9ac2ef3e64",
+    "url": "/portfolio/static/media/UbuntuMono-B.0734aff7.ttf"
+  },
+  {
+    "revision": "ace87566a523960287b20a3f9c064cb9",
+    "url": "/portfolio/static/media/FFFFORWA.ace87566.TTF"
+  },
+  {
+    "revision": "2292827f4aee8916ebdc860c02520790",
+    "url": "/portfolio/static/media/code-head-bg.2292827f.jpg"
+  },
+  {
+    "revision": "f5575047c43c4e76bb3cef25f66f6541",
+    "url": "/portfolio/static/media/allin.f5575047.jpg"
+  },
+  {
+    "revision": "cd9837dfcb80cce7a7f22f5892abeab4",
+    "url": "/portfolio/static/media/animated-ringing-phone.cd9837df.gif"
+  },
+  {
+    "revision": "57cd5e0779b76c39ad5786428b320ed5",
+    "url": "/portfolio/static/media/chat.57cd5e07.jpg"
+  },
+  {
+    "revision": "d6fe54c90b565b2db691bf6860786f89",
+    "url": "/portfolio/static/media/head-bg-1.d6fe54c9.webp"
+  },
+  {
+    "revision": "ff773842a79f60afb5dcdf5640121e4f",
+    "url": "/portfolio/static/media/kc-beauty.ff773842.png"
+  },
+  {
+    "revision": "8b18a721e898167b7285ed5e9380432b",
+    "url": "/portfolio/static/media/preview-project.8b18a721.png"
+  },
+  {
+    "revision": "154674cfde21005447e6655438e724ec",
+    "url": "/portfolio/static/media/supermarket.154674cf.jpg"
+  },
+  {
+    "revision": "9d28b6ab38bf295eff1fc1261d7c6889",
+    "url": "/portfolio/static/media/supermarket.9d28b6ab.png"
+  },
+  {
+    "revision": "3917761e790cceb1bfa5b6dc4afa082e",
+    "url": "/portfolio/static/media/tasks-board.3917761e.png"
+  },
+  {
+    "revision": "23928d7a49dec35537a560b7d3eea194",
+    "url": "/portfolio/static/media/vacations.23928d7a.jpg"
+  },
+  {
+    "revision": "5247fafffb84ca9380ee634a55140795",
+    "url": "/portfolio/static/media/vacations.5247faff.png"
+  },
+  {
+    "revision": "76ba4c0b5ab046845ffbc4bce0be2aa2",
+    "url": "/portfolio/static/media/yoga.76ba4c0b.jpg"
+  },
+  {
+    "revision": "8797adc64c75cd5aed314cc7d61eaf85",
+    "url": "/portfolio/static/media/allin-preview.8797adc6.mp4"
+  },
+  {
+    "revision": "e3ca718353ad79510b9e79046236315b",
+    "url": "/portfolio/static/media/allin-video.e3ca7183.mp4"
+  },
+  {
+    "revision": "41c27baaa93ca8e0e2f27e21544616d2",
+    "url": "/portfolio/static/media/chat-video.41c27baa.mp4"
+  },
+  {
+    "revision": "8638d05a10e72da805056486fa118d98",
+    "url": "/portfolio/static/media/kc-video.8638d05a.mp4"
+  },
+  {
+    "revision": "59c8a7b4c8887e5e415a0a991c791269",
+    "url": "/portfolio/static/media/super.59c8a7b4.mp4"
+  },
+  {
+    "revision": "47b102ceaf40ff6533dc693d2f1aefce",
+    "url": "/portfolio/static/media/vacations.47b102ce.mp4"
+  },
+  {
+    "revision": "3304ff18f8331f1718385f1df4b246d3",
+    "url": "/portfolio/static/media/yoga-video.3304ff18.mp4"
+  },
+  {
+    "revision": "b3325255040d4c2660c91f7ac7fc2a75",
+    "url": "/portfolio/index.html"
+  }
+];
